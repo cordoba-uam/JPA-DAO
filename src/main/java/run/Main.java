211c99fe50098319f848b550d36be20c31985e2a
cordoba-uam <scordobag@uamv.edu.ni>
2025-10-22 16:21:13 -0600

    En la persistencia, las clases ya fueron agregadas, mientras que en Main, ya se hace el CRUD.

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -4,12 +4,51 @@ import entities.*;
 import services.interfaces.ICRUD;
 import services.dao.MyDao;
 
+import java.util.List;
+
 public class Main {
 
     public static final ICRUD dao = new MyDao();
 
+    public static void insertarAutor() {
+        Autor autor = new Autor();
+        autor.setNombre("Gabriel García Márquez");
+        autor.setNacionalidad("Colombiana");
+        dao.insert(autor);
+
+        Autor autor2 = new Autor();
+        autor2.setNombre("Isabel Allende");
+        autor2.setNacionalidad("Colombiana");
+        dao.insert(autor2);
+    }
+
+    public static void listarAutores(){
+
+        System.out.println("Registros almacenados");
+        List<Autor> autores = dao.getALL("autores.ALL", Autor.class);
+        autores.forEach(autor -> System.out.println(autor.getNombre()));
+    }
+
+    public static void actualizarAutor(){
+        Autor autor = new Autor();
+        autor = dao.findById(1, Autor.class);
+        autor.setNacionalidad("Argentina");
+        dao.update(autor);
+    }
+
+    public static void eliminarAutor(){
+        Autor autor = new Autor();
+        autor = dao.findById(2, Autor.class);
+        dao.delete(autor);
+    }
+
     // Punto de entrada de la aplicación
     public static void main(String[] args) {
-
+        insertarAutor();
+        listarAutores();
+        actualizarAutor();
+        listarAutores();
+        eliminarAutor();
+        listarAutores();
     }
 }
