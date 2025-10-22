eedabcf970d3bcdeba1ad65e4d8fa18d124c0402
cordoba-uam <scordobag@uamv.edu.ni>
2025-10-22 16:29:07 -0600

    For each arreglado

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -26,7 +26,10 @@ public class Main {
 
         System.out.println("Registros almacenados");
         List<Autor> autores = dao.getALL("autores.ALL", Autor.class);
-        autores.forEach(autor -> System.out.println(autor.getNombre()));
+        for (Autor a : autores){
+
+            System.out.println(a.getNombre());
+        }
     }
 
     public static void actualizarAutor(){
