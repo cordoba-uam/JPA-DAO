fe317d7cd57f5cf5609d89f9900584f72ad8c9e3
cordoba-uam <scordobag@uamv.edu.ni>
2025-10-22 15:32:04 -0600

    Agrega 'JPAConexion', dentro del paquete 'util', para el EntityManager

---
+++ src/main/java/util/JPAConexion.java
@@ -0,0 +1,20 @@
+package util;
+
+import jakarta.persistence.EntityManagerFactory;
+import  jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+public class JPAConexion {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-db");
+
+    public static EntityManager getEntityManager() {
+
+        return emf.createEntityManager();
+    }
+
+    public static void close() {
+
+        emf.close();
+    }
+}
