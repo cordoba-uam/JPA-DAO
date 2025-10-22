419566b84233bb0a3fa3f56f248d3a193fd02b2a
cordoba-uam <scordobag@uamv.edu.ni>
2025-10-22 16:11:27 -0600

    Inicialización del 'Main'. Incluye el 'Dao', las importaciones, y la función 'Main'.

---
+++ src/main/java/run/Main.java
@@ -0,0 +1,15 @@
+package run;
+
+import entities.*;
+import services.interfaces.ICRUD;
+import services.dao.MyDao;
+
+public class Main {
+
+    public static final ICRUD dao = new MyDao();
+
+    // Punto de entrada de la aplicación
+    public static void main(String[] args) {
+
+    }
+}
